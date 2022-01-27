fcda7ba5c1f5a99bc359703ec76cd5abcdb002fb
Biniam Daniel <biniamdnl@gmail.com>
2022-01-27 17:17:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// assign office number
+const officeNumber = 7058;
+
+//assign mobile number
+const mobileNumber = 3346
+
+// get the result
+const Result = officeNumber / mobileNumber;
+
+//log the output
+console.log(Result)
+
+// IT IS WORKING PERFECTLY :)
